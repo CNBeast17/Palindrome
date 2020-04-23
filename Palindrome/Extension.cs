a58e0d0cec889ef3a5c3515adffc0395ecdf1274
Philani Mlawu <philanijustin@gmail.com>
2020-04-23 17:31:22 +0200

    Commit implementation

---
+++ Palindrome/Extension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    static class Extension
+    {
+        public static bool IsPalindrome(string word)
+        {   
+            word = word.ToLower();
+            int wordLen = word.Length;
+            int wordHalfLen = wordLen / 2;
+            for (int i = 0; i < wordHalfLen; i++)
+            {
+                if(word.Substring(i,1) != word.Substring(wordLen-1,1))
+                {
+                    //Testing corresponding characters allows for a quicker exit if the word
+                    //is not a pilandrome
+                    //No time is wasted testing the rest of the word after
+                    return false;
+                }
+                wordLen--;
+            }
+            return true;           
+        }
+    }
+}
--- Palindrome/Program.cs
+++ Palindrome/Program.cs
@@ -10,6 +10,20 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a word");
+            string word = Console.ReadLine();
+
+            bool palindrome = Extension.IsPalindrome(word);
+
+            if(palindrome)
+            {
+                Console.WriteLine("Is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("Is not palindrome");
+            }
+            Console.ReadKey();
         }
     }
 }
